3ff0c575194d9d06d558181c80e2df112215d6cb
ale <alemoura.zyc@gmail.com>
2024-09-27 07:27:14 -0300

    feat: create addDistroList function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+const debianList = [
+    { tag: 'h2', text: 'Debian based' },
+    { tag: 'li', text: 'Ubuntu' },
+    { tag: 'li', text: 'Linux Mint' },
+    { tag: 'li', text: 'Raspbian' },
+    { tag: 'li', text: 'Kali Linux' },
+    { tag: 'li', text: 'MX Linux' }
+];
+
+const fedoraList = [
+    { tag: 'h2', text: 'Fedora based' },
+    { tag: 'li', text: 'Red Hat Enterprise Linux (RHEL)' },
+    { tag: 'li', text: 'CentOS Stream' },
+    { tag: 'li', text: 'Silverblue' },
+    { tag: 'li', text: 'Fedora Kinoite' },
+    { tag: 'li', text: 'Feren OS' }
+];
+
 function createContainerList () {
     const container = document.querySelector('.container');
     const containerList = document.createElement('div');
@@ -18,5 +36,23 @@ function createDistroList () {
     containerList.appendChild(fedora);
 }
 
+function addDistroList () {
+    const debianDiv = document.querySelector('.debian');
+    const fedoraDiv = document.querySelector('.fedora');
+
+    for (let i = 0; i < debianList.length; i++) {
+        const item = document.createElement(debianList[i].tag);
+        item.innerText = debianList[i].text;
+        debianDiv.appendChild(item);
+    }
+
+    for (let i = 0; i < fedoraList.length; i++) {
+        const item = document.createElement(fedoraList[i].tag);
+        item.innerText = fedoraList[i].text;
+        fedoraDiv.appendChild(item);
+    }
+}
+
 createContainerList();
 createDistroList();
+addDistroList();
