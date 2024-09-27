291953052dafd5a77b650bde15ca10ea6623b560
ale <alemoura.zyc@gmail.com>
2024-09-27 07:26:26 -0300

    feat: create createDistroList function

--- js/main.js
+++ js/main.js
@@ -6,4 +6,17 @@ function createContainerList () {
     container.appendChild(containerList);    
 }
 
+function createDistroList () {
+    const containerList = document.querySelector('.container-list');
+    const debian = document.createElement('div');
+    const fedora = document.createElement('div');
+    
+    debian.className = 'debian';
+    fedora.className = 'fedora';
+    
+    containerList.appendChild(debian);
+    containerList.appendChild(fedora);
+}
+
 createContainerList();
+createDistroList();
